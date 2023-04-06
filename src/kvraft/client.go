5314be62f47f658f83a68812408102118d7aead6
Lucius <souzou@foxmail.com>
2023-04-06 16:33:21 +0800

    Polish Lab 3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,19 +3,15 @@ package kvraft
 import (
 	"crypto/rand"
 	"math/big"
-	"sync"
 
 	"6.5840/labrpc"
 )
-
-const Null = -1
 
 type Clerk struct {
 	servers   []*labrpc.ClientEnd
 	leaderId  int
 	id        int64
 	requestId int
-	mu        sync.Mutex
 	// You will have to modify this struct.
 }
 
@@ -29,7 +25,6 @@ func nrand() int64 {
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := &Clerk{
 		servers:   servers,
-		leaderId:  Null,
 		id:        nrand(),
 		requestId: 0,
 	}
@@ -49,43 +44,24 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
 	// You will have to modify this function.
-	value := ""
-	ck.mu.Lock()
 	args := GetArgs{Key: key, ClientId: ck.id, RequestId: ck.requestId}
 	ck.requestId++
-	defer ck.mu.Unlock()
-	if ck.leaderId != Null {
-		reply := GetReply{}
-		ck.mu.Unlock()
-		ok := ck.servers[ck.leaderId].Call("KVServer.Get", &args, &reply)
-		ck.mu.Lock()
-		if !ok || reply.Err == ErrWrongLeader {
-			ck.leaderId = Null
-		} else if reply.Err == OK {
-			value = reply.Value
-		}
+	reply := GetReply{}
+	ok := ck.servers[ck.leaderId].Call("KVServer.Get", &args, &reply)
+	if ok && reply.Err != ErrWrongLeader {
+		return reply.Value
 	}
-	if ck.leaderId == Null {
-		loop := true
-		n := len(ck.servers)
-		for loop {
-			for i := 0; i < n; i++ {
-				reply := GetReply{}
-				ck.mu.Unlock()
-				ok := ck.servers[i].Call("KVServer.Get", &args, &reply)
-				ck.mu.Lock()
-				if ok && reply.Err != ErrWrongLeader {
-					if reply.Err == OK {
-						value = reply.Value
-					}
-					ck.leaderId = i
-					loop = false
-					break
-				}
+	n := len(ck.servers)
+	for {
+		for i := 0; i < n; i++ {
+			reply := GetReply{}
+			ok := ck.servers[i].Call("KVServer.Get", &args, &reply)
+			if ok && reply.Err != ErrWrongLeader {
+				ck.leaderId = i
+				return reply.Value
 			}
 		}
 	}
-	return value
 }
 
 // shared by Put and Append.
@@ -98,33 +74,21 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
-	ck.mu.Lock()
 	args := PutAppendArgs{Key: key, Value: value, Op: op, ClientId: ck.id, RequestId: ck.requestId}
 	ck.requestId++
-	defer ck.mu.Unlock()
-	if ck.leaderId != Null {
-		reply := PutAppendReply{}
-		ck.mu.Unlock()
-		ok := ck.servers[ck.leaderId].Call("KVServer.PutAppend", &args, &reply)
-		ck.mu.Lock()
-		if !ok || reply.Err == ErrWrongLeader {
-			ck.leaderId = Null
-		}
+	reply := PutAppendReply{}
+	ok := ck.servers[ck.leaderId].Call("KVServer.PutAppend", &args, &reply)
+	if ok && reply.Err != ErrWrongLeader {
+		return
 	}
-	if ck.leaderId == Null {
-		loop := true
-		n := len(ck.servers)
-		for loop {
-			for i := 0; i < n; i++ {
-				reply := PutAppendReply{}
-				ck.mu.Unlock()
-				ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
-				ck.mu.Lock()
-				if ok && reply.Err != ErrWrongLeader {
-					ck.leaderId = i
-					loop = false
-					break
-				}
+	n := len(ck.servers)
+	for {
+		for i := 0; i < n; i++ {
+			reply := PutAppendReply{}
+			ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+			if ok && reply.Err != ErrWrongLeader {
+				ck.leaderId = i
+				return
 			}
 		}
 	}
